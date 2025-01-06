2f7486254c2a6c1a8ce5380ece3da93d55f7b766
KirillBal <kirrbalykov1955@gmail.com>
2025-01-06 09:52:29 +0000

    2_EG

---
+++ 2.py
@@ -0,0 +1,8 @@
+Print ("x y z w")
+for x in range (0,1):
+ for y in range (0,1):
+  for z in range (0,1):
+   for w in range (0,1):
+    if (not(w) or z)and((not(y) or x) == (not(z) or y)):
+     print (x y z w)
+
